ccbccc6ab268c44a73fd245407556d7687b744dc
耿越 <gengyue2468@outlook.com>
2025-10-07 14:44:22 +0800

    如怜悯罪人

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -8,5 +8,6 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_SECRET,
     }),
   ],
+  secret: process.env.NEXTAUTH_SECRET,
   debug: process.env.NODE_ENV === "development",
 });
